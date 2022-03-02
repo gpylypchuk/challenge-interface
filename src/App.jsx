75d29bf016f35b6cf56fedc78d1a8046614b06af
geroo <geronimopylypchuk@gmail.com>
2022-03-02 20:21:07 -0300

    feat: provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
-import { Button, Stack } from '@chakra-ui/react'
+import { Button, Stack, Heading, Text } from '@chakra-ui/react'
+import { useEffect } from 'react';
 
-function App() {
+const App = () => {
 
   const ETH_POOL = '0xA5de4364e621f37F73Dfa6fCcd905aA427aE192a';
+  let userAddress;
 
-  function connectWallet() {
+  useEffect(() => {
+    let provider = window.ethereum;
+		if(typeof provider !== 'undefined') {
+      provider.request({ method: 'eth_requestAccounts' }).then((accounts) => {
+        selectedAccount = accounts[0];
+				document.getElementById('account').innerHTML = selectedAccount;
+      });
+      window.ethereum.on('accountsChanged', (accounts) => {
+        selectedAccount = accounts[0];
+				document.getElementById('account').innerHTML = selectedAccount;
+      });
+    }
+  }, []);
+
+  const connectWallet = async () => {
     if (typeof window.ethereum !== 'undefined') {
-      console.log('MetaMask is installed!');
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      userAddress = accounts[0];
+      document.getElementById('account').innerHTML = userAddress;
     }
   }
 
   return (
     <div>
-      <Stack spacing={4} direction='row' align='center'>
-        <Button colorScheme='green' variant='outline' size='md' onClick={connectWallet}>
+      <Stack spacing={4} direction='column' align='center'>
+        <Heading as='h2' size='3xl' isTruncated>
+          Ethereum Pool
+        </Heading>
+        <Button marginTop='10%' colorScheme='green' variant='outline' size='md' marginLeft='50%' marginRight='50%' onClick={connectWallet}>
           Connect Wallet
         </Button>
-        <Button colorScheme='red' variant='outline' size='md'>
-          Disconnect
-        </Button>
+        <Text color='gray.500' isTruncated id='account'></Text>
       </Stack>
     </div>
   )
